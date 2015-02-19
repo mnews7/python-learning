92b3a596e524402c22307a51a2d76ed2ad1c88c4
Matthew Newsom <mnews7@users.noreply.github.com>
2015-02-19 10:43:35 -0500

    Create _hey_.py
    Was originally nested for loops, broke it out to try to introduce multiprocessing.

---
+++ _hey_.py
@@ -0,0 +1,33 @@
+__author__ = 'Matthew'
+
+# Prints out a list of fibonnaci numbers divisible by 3 between 0 and n
+
+def fib(n):
+    result = []
+
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a + b
+
+    size = len(result)
+    for num in range(1, size):
+        divis = (division(result[num], n))
+        print('Fibonacci number', result[num], 'is divisible by', divis)
+
+
+def division(reslist, n):
+    divi = []
+
+    for div in range(2, n):
+        if reslist <= div:
+            break
+        else:
+            if reslist % div == 0:
+                divi.append(div)
+    return (divi)
+
+
+# Calling the scripts
+f100 = fib(10000000000)
+print(f100)
